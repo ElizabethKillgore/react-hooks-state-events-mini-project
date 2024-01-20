27e262b74d1aa9f723b82ebd1d1888194a680388
Elizabeth <ek@elizabethkillgore.com>
2024-01-20 16:34:21 -0500

    react hooks state events mini project

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,38 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTASKS] = useState(TASKS)
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  
+
+  function deleteTask(text) {
+    const newList = tasks.filter((task) => task.text !==  text )
+    setTASKS(newList)
+  }
+
+  function handleSelectedButton(clickedCategory) {
+    setSelectedCategory(clickedCategory)
+  }
+  
+  const filteredTasks = tasks.filter((task) => {
+    if (selectedCategory === "All") {
+      return task 
+    } else {
+      return task.category === selectedCategory }
+
+    }    
+  )
+  
+  function onTaskFormSubmit(newTask) {
+    setTASKS([...tasks, newTask])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} handleSelectedButton={handleSelectedButton} selectedCategory={selectedCategory}/>
+      <NewTaskForm categories={CATEGORIES} tasks={tasks} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={filteredTasks} deleteTask={deleteTask}/>
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm( { CATEGORIES, tasks, onTaskFormSubmit }) {
+
+  const [newTaskName, setNewTaskName] = useState("")
+  const [taskCategory, setTaskCategory] = useState("Code")
+  
+  function handleTaskFormSubmit(e) {
+    e.preventDefault()
+    const newTask = {
+       
+      text: newTaskName,
+      category: taskCategory
+    }
+    onTaskFormSubmit(newTask)
+    console.log(newTask)
+  }
+
+  function handleNewTask(e) {
+    setNewTaskName(e.target.value)
+  }
+
+
+  function handleTaskCategory(e) {
+    setTaskCategory(e.target.value)
+  }
+  
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleTaskFormSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={newTaskName} onChange={handleNewTask}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChange={handleTaskCategory} >
+          <option value="Code">Code</option>
+          <option value="Food">Food</option>
+          <option value="Money">Money</option>
+          <option value="Misc">Misc</option>
         </select>
       </label>
       <input type="submit" value="Add task" />
